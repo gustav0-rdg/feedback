73ed7cc432c60e7c5c2828df222976ad1af1d0ab
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-02-25 10:10:02 -0300

    add: nova rota para cadastrar os comentarios

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ app = Flask(__name__)
 def pagina_inicial():
     return render_template("index.html")
 
+# Rota para pegar as informações do formulario
+
+@app.route("/post/comentarios", methods=["POST"])
+def cadastrar_comentario():
+    return redirect("/")
 app.run(debug=True, host="0.0.0.0", port=8080)
