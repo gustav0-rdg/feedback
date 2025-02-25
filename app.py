ec8694ad32a18abd1fb85a64250401ca43fa4440
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-02-25 08:22:04 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# Importando tudo que vai ser utilizado pelo Flask
+from flask import Flask, render_template, request, redirect
+
+# Criando a variavel para instanciar o Flask
+app = Flask(__name__)
+
+# Rota inicial
+@app.route("/", methods=["GET"])
+def pagina_inicial():
+    return 'co'
+
+app.run(debug=True, host="0.0.0.0", port=8080)
