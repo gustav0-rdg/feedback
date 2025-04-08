6b61532c856e845518e927d66f5dd3ed2714cf46
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-04-08 11:01:15 -0300

    nova conexao com deploy

--- data/conexao.py
+++ data/conexao.py
@@ -5,8 +5,8 @@ class Conexao:
     # Criando conexao
     def criar_conexao():
         conexao = mysql.connector.connect(
-            host="10.110.134.2",
-            port=3306,
+            host="bd-godofredo-alexstocco-93db.b.aivencloud.com",
+            port=27974,
             user="3ds",
             password="banana",
             database = "db_feedback"
